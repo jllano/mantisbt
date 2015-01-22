1e8365b82600a3f52bb02cb75e84c1e563df11aa
Victor Boctor <victor@mantishub.net>
2015-01-22 07:30:24 -0800

    Handle case where trial_conversion_url is not defined
    Mainly when mantishub info file is doesn't exist on the instance yet or on development machines.

--- core/mantishub_api.php
+++ core/mantishub_api.php
@@ -35,8 +35,8 @@ function mantishub_top_message() {
 	if ( $g_mantishub_info_trial && current_user_is_administrator() ) {
 		$t_issues_count = mantishub_table_row_count( 'bug' );
 
-		if ( $t_issues_count >= 5 ) {
-			$t_trial_conversion_url = config_get( 'mantishub_info_trial_conversion_url' );
+		$t_trial_conversion_url = config_get( 'mantishub_info_trial_conversion_url', '' );
+		if ( $t_issues_count >= 5 && !is_blank( $t_trial_conversion_url ) ) {
 			echo '<div style="background-color: #fff494; z-index: 10; position: absolute; right: 5px; top: 5px; text-align: right;"><b>Trial Version:</b> Click <a href="' . $t_trial_conversion_url . '" target="_blank">here</a> to convert to paid and enable daily backups.</div>';
 		}
 	}
