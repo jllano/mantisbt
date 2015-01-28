479f2b4498e000989d7b74549ee4019de11f5234
Rafik Robeal <rafik@mantishub.net>
2015-01-27 21:38:40 -0800

    Fix error page rendering errors
    Fixes #307: Broken error when viewing Import CSV tab with all prjects

--- core/error_api.php
+++ core/error_api.php
@@ -76,7 +76,7 @@ function error_handler( $p_type, $p_error, $p_file, $p_line, array $p_context ) 
 	global $g_error_parameters, $g_error_handled, $g_error_proceed_url;
 	global $g_error_send_page_header;
 
-	# check if errors were disabled with @ somewhere in this call chain
+	# check if errors were disabled with @ somewhere in this call chailayout_page_header_endn
 	if( 0 == error_reporting() ) {
 		return;
 	}
@@ -215,12 +215,13 @@ function error_handler( $p_type, $p_error, $p_file, $p_line, array $p_context ) 
 							layout_page_begin();
 						}
 					} else {
-						echo '<html><head><title>', $t_error_type, '</title></head><body>';
+                        layout_page_header( $t_error_type );
 					}
 				} else {
 					# Output the previously sent headers, if defined
 					if( isset( $t_old_headers ) ) {
 						echo $t_old_headers, "\n";
+                        layout_page_header_end();
 						layout_page_begin();
 					}
 				}
