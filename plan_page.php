d397c30fc97ad74ce66372be4853892372458691
Victor Boctor <victor@mantishub.net>
2015-11-20 20:37:41 -0800

    Show team member access level on plan page

--- plan_page.php
+++ plan_page.php
@@ -8,10 +8,10 @@ auth_reauthenticate();
 
 access_ensure_global_level( ADMINISTRATOR );
 
-html_page_top( 'Plan' );
-print_manage_menu( 'plan_page.php' );
-
 function print_plan_form_header( $p_title ) {
+	html_page_top( lang_get( 'mantishub_plan_menu_option' ) );
+	print_manage_menu( 'plan_page.php' );
+	echo '<br />';
 	echo '<div class="form-container">';
 	echo '<fieldset>';
 	echo '<legend><span>' . $p_title . '</span></legend>';
@@ -73,11 +73,12 @@ print_field( lang_get( 'mantishub_plan_projects_count' ), sprintf( $t_value_of_l
 print_field( lang_get( 'mantishub_plan_issues_count' ), sprintf( $t_value_of_limit, plan_issues_count(), plan_max_issues_string() ) );
 print_field( lang_get( 'mantishub_plan_users_count' ), sprintf( $t_value_of_limit, plan_users_count(), plan_max_users_string() ) );
 print_field( lang_get( 'mantishub_plan_attachments_count' ), sprintf( $t_value_of_limit, plan_attachments_count(), plan_max_attachments_string() ) );
-
 $t_team_members_value = sprintf( $t_value_of_limit, $t_result['count'], plan_max_team_members_string() );
+$t_team_members_value .= ' ' . sprintf( lang_get( 'mantishub_plan_assign_access_level' ), MantisEnum::getLabel( lang_get( 'access_levels_enum_string' ), $t_result['access_level'] ) );
+
 $t_team_packs_needed = plan_user_packs_needed( $t_result['count'] );
 if ( $t_team_packs_needed > 0 ) {
-	$t_team_members_value .= sprintf( ' (%d team packs)', $t_team_packs_needed );
+	$t_team_members_value .= '<br />' . sprintf( lang_get( 'mantishub_plan_team_packs' ), $t_team_packs_needed );
 }
 
 $t_team_members_value .= '<br /><br />' . team_info( $t_result );
