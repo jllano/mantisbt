14bf26c2c52e98276d31043a7b87002b5a7584bf
Victor Boctor <vboctor@gmail.com>
2016-01-29 04:32:03 +0000

    Add Mantishub plugin with sign-in on install
    - Add a Mantishub plugin that is always enabled.
    - Add auto-sign in functionality for new installations.

---
+++ plugins/MantisHub/LICENSE.md
@@ -0,0 +1,6 @@
+MantisHub Plugin
+----------------
+
+This plugin is owned and licensed exclusively to MantisHub.
+It is not to be used by any individual or organization without
+a written consent from MantisHub LLC.
---
+++ plugins/MantisHub/pages/sso.php
@@ -0,0 +1,20 @@
+<?php
+/**************************************************************************
+ MantisHub Plugin
+ Copyright (c) MantisHub - Victor Boctor
+ All rights reserved.
+ **************************************************************************/
+
+require_api( 'mantishub_api.php' );
+require_once( dirname( dirname( __FILE__ ) ) . '/core/mantishub_plugin_api.php' );
+
+$f_token = gpc_get_string( 'token', '' );
+
+if ( mantishub_login_to_new_instance( $f_token ) ) {
+    $t_return = string_url( string_sanitize_url( config_get( 'default_home_page' ) ) );
+    $t_redirect_url = 'login_cookie_test.php?return=' . $t_return;
+} else {
+    $t_redirect_url = 'login_page.php';
+}
+
+print_header_redirect( $t_redirect_url );
