0bf06b81357b30a9061fb7edd214d1b8ad67a6be
Victor Boctor <victor@mantishub.net>
2016-02-20 18:31:06 -0500

    Fix auto-creation of first project

--- plugins/MantisHub/pages/sso.php
+++ plugins/MantisHub/pages/sso.php
@@ -10,7 +10,11 @@ require_once( dirname( dirname( __FILE__ ) ) . '/core/mantishub_plugin_api.php' 
 
 $f_token = gpc_get_string( 'token', '' );
 
-if ( mantishub_login_to_new_instance( $f_token ) ) {
+if( mantishub_login_to_new_instance( $f_token ) ) {
+    if( project_table_empty() ) {
+        project_create( /* name */ 'MyProject', /* desc */ '', /* status: stable */ 50 );
+    }
+
     $t_return = string_url( string_sanitize_url( config_get( 'default_home_page' ) ) );
     $t_redirect_url = 'login_cookie_test.php?return=' . $t_return;
 } else {
