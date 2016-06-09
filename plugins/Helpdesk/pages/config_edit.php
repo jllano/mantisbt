b42cdd428cb647468ba2e7dd2d7ce6825a754942
Victor Boctor <victor@mantishub.net>
2016-06-08 23:00:34 -0700

    Always save configs + redirect to plugins page
    - Redirect to plugins page after saving configs
    - Always set configs even if they match defaults.

--- plugins/Helpdesk/pages/config_edit.php
+++ plugins/Helpdesk/pages/config_edit.php
@@ -2,17 +2,11 @@
 form_security_validate( 'plugin_Helpdesk_config' );
 access_ensure_global_level( config_get( 'manage_plugin_threshold' ) );
 
-function config_option_update( $name, $value ) {
-	if ( $value != plugin_config_get( $name ) ) {
-		plugin_config_set( $name, $value );
-	}
-}
-
-config_option_update( 'enabled', gpc_get_bool( 'enabled' ) );
-config_option_update( 'default_project', gpc_get_int( 'default_project' ) );
-config_option_update( 'issue_reported_message', gpc_get( 'issue_reported_message' ) );
-config_option_update( 'failed_message', gpc_get( 'failed_message' ) );
+plugin_config_set( 'enabled', gpc_get_bool( 'enabled' ) ? 1 : 0 );
+plugin_config_set( 'default_project', gpc_get_int( 'default_project' ) );
+plugin_config_set( 'issue_reported_message', gpc_get( 'issue_reported_message' ) );
+plugin_config_set( 'failed_message', gpc_get( 'failed_message' ) );
 
 form_security_purge( 'plugin_Helpdesk_config' );
-print_successful_redirect( plugin_page( 'config', true ) );
+print_successful_redirect( 'manage_plugin_page.php' );
 
