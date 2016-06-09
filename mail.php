d0c90528e6400ca6ad699e1598110702fa06ecbb
Victor Boctor <victor@mantishub.net>
2016-06-08 22:59:45 -0700

    Set plugin name before redirecting to Helpdesk

--- mail.php
+++ mail.php
@@ -1,4 +1,8 @@
 <?php
+require_once( 'core.php' );
+require_api( 'plugin_api.php' );
+
 # redirect to helpdesk plugin
+plugin_push_current( 'Helpdesk' );
 include( dirname( __FILE__ ) . '/plugins/Helpdesk/pages/mail.php' );
 
