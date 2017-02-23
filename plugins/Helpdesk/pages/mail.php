85a0affb2cd7300b942912f4fcb6cc5f56318d8f
Teodor Talov <teodor@talov.com>
2017-02-23 02:31:27 -0500

    Excluded from email from notifications

--- plugins/Helpdesk/pages/mail.php
+++ plugins/Helpdesk/pages/mail.php
@@ -59,14 +59,23 @@ function mantishub_collect_additional_recipients( $f_additional_recipients_heade
 		}
 	);
 
-	$t_emails = array_unique( $t_emails );
-	$t_exclude_emails = plugin_config_get( 'exclude_emails' );
-	if( !empty( $t_exclude_emails ) ) {
-		if( !is_array( $t_exclude_emails ) ) {
-			$t_exclude_emails = array( $t_exclude_emails );
-		}
-		$t_emails = array_diff( $t_emails, $t_exclude_emails );
+	$t_exclude_emails = array();
+
+	$from_email = config_get( 'from_email' );
+	if(!empty($from_email)){
+		$t_exclude_emails[] = $from_email;
 	}
+
+	$t_exclude_addional_emails = plugin_config_get( 'exclude_emails' );
+	if( !empty( $t_exclude_addional_emails ) ) {
+		if( !is_array( $t_exclude_addional_emails ) ) {
+			$t_exclude_addional_emails = array( $t_exclude_addional_emails );
+		}
+		$t_exclude_emails = array_merge($t_exclude_emails, $t_exclude_addional_emails);
+	}
+
+	$t_emails = array_diff( $t_emails, $t_exclude_emails );
+	$t_emails = array_unique( $t_emails );
 
 	return $t_emails;
 }
