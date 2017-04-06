b1ef8825292fa390e985586a0bef7d9b13c9b863
Joel C. Llano <joel@mantishub.com>
2017-04-06 15:15:23 +0800

    adding skeleton template for main page

---
+++ kb/main.php
@@ -0,0 +1,32 @@
+<html dir="ltr" lang="en-US"><head>
+  <meta charset="utf-8">
+  <!-- v11709 -->
+  <title>MantisHub</title>
+
+  
+</head>
+
+<body>
+
+<div class="table-responsive">
+	<table class="table">
+		<thead>
+			<tr>
+				<th> Summary </th>
+				<th> Last Updated </th>
+				<th> Category </th>
+			</tr>
+		</thead>
+
+		<tbody>		
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+
+</body>
+</html>
